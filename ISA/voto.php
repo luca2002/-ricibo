c7d50d1cccf175831cfec0e5fc1af362ca06bf51
robalb <alberto.ventafridda@gmail.com>
2018-02-18 15:35:22 +0100

    migliorato , e aggihnto eco del codice di conferma anche in reinvio mail

--- ISA/voto.php
+++ ISA/voto.php
@@ -57,9 +57,7 @@ else{//il database è connesso
 				echo 'votazione andata a buon fine. clicca sul link inviato al tuo indirizzo mail per completare<br>';
 				echo '<a href="voto.php?resend='.$email.'">reinvia mail</a>';
 				//TODO: invio mail
-				$TESTO = 'conferma il tuo voto cliccando su questo link';
-				echo $codiceConferma;
-				echo inviaMail('noreply@mailfarlocca.it',$email,'RICIBO conferma mail per la votazione',$TESTO);
+				echo $codiceConferma;//debug
 			}else{
 				echo 'errore durante la registrazione del tuo voto';
 			}
@@ -91,10 +89,20 @@ else{//il database è connesso
 				$verificato = $row['verificato'];
 			}
 			if($verificato == 0){//la mail non è registrata
-				//TODO
-				$TESTO = 'conferma il tuo voto cliccando su questo link';
-				echo inviaMail('noreply@mailfarlocca.it',$email,'RICIBO conferma mail per la votazione',$TESTO);
-				echo 'ok, mail reinviata';
+				//genera il codice di conferma di otto cifre
+				$codiceConferma = rand(1,9);
+				for($i=0; $i<7; $i++) {
+					$codiceConferma .= rand(0,9);
+				}
+				//aggiorna il codice di conferma nel db
+				$query = "UPDATE concorsologo_votanti SET codice_conferma = '$codiceConferma' WHERE concorsologo_votanti.email = '$email'";
+				if(mysqli_query($db,$query)){
+					echo 'mail reinviata!<br>';
+					//TODO: invio mail
+					echo $codiceConferma;//debug
+				}else{
+					echo 'errore durante il reinvio della mail';
+				}	
 			}else{
 				echo 'votazione già confermata';
 			}
