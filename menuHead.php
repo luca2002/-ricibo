bbbc3a65b9d4aa2327ef69090af68a9f1a9d468f
robalb <alberto.ventafridda@gmail.com>
2018-02-18 17:01:06 +0100

    aggiornato jquery

--- menuHead.php
+++ menuHead.php
@@ -6,6 +6,6 @@
 <link type="text/css" rel="stylesheet" href="/~ricibo/css/materialize.css"  media="screen,projection"/>
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/jquery-3.3.1.js"></script>
 <script type="text/javascript" src="/~ricibo/js/materialize.min.js"></script>
 <script type="text/javascript" src="/~ricibo/js/init.js"></script>
