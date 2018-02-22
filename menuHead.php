f5436b76d283df5ab5480e741be4042ec082feb0
robalb-scuola <36276862+slendy666@users.noreply.github.com>
2018-02-22 15:24:17 +0100

    modificato head

--- menuHead.php
+++ menuHead.php
@@ -1,11 +1,11 @@
 <title> RiCibo </title>
-<link rel="shortcut icon" href="/~ricibo/img/favicon.ico" />
+<link rel="shortcut icon" href="/~ricibo14/img/favicon.ico" />
 <!--Import Google Icon Font-->
 <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <!--Import materialize.css-->
-<link type="text/css" rel="stylesheet" href="/~ricibo/css/materialize.css"  media="screen,projection"/>
+<link type="text/css" rel="stylesheet" href="/~ricibo14/css/materialize.css"  media="screen,projection"/>
 <!--Let browser know website is optimized for mobile-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="/~ricibo/js/materialize.min.js"></script>
-<script type="text/javascript" src="/~ricibo/js/init.js"></script>
+<script type="text/javascript" src="/~ricibo14/js/materialize.min.js"></script>
+<script type="text/javascript" src="/~ricibo14/js/init.js"></script>
