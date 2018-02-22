58cfe4a3d3c5ded28e635ec4c68d14fc5253e58d
paolocipolla <36734553+paolocipolla@users.noreply.github.com>
2018-02-22 15:24:05 +0100

    modificato database

--- funzioni/oDBConn.php
+++ funzioni/oDBConn.php
@@ -2,9 +2,9 @@
 function oDBConn() {
 	// INFO DB
 	$DBhost = "localhost";  // "93.46.220.3";
-	$DBuser = "root";
-	$DBpass = "";
-	$DBName = "ricibo";
+	$DBuser = "ricibo14";
+	$DBpass = "ricibo14";
+	$DBName = "ricibo14";
 	$codUscita = 0;
 	$codUscita = mysqli_connect($DBhost,$DBuser,$DBpass,$DBName);
 	if (!$codUscita) { $codUscita = 1; } // Errore: connessione al DB fallita
